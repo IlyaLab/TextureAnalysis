cf38d0b1e779dbb136026433350fabf085ee40e7
NSwanson216 <137955372+NSwanson216@users.noreply.github.com>
2025-03-12 13:19:07 -0700

    Update FeatureExtraction.py

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -12,6 +12,7 @@ import pandas as pd
 import time
 import re
 from multiprocessing import Process
+import warnings
 
 import skimage.color
 
@@ -27,14 +28,16 @@ angle_names = ['0', '45', '90', '135']
 distances = [1]
 angles = [0, np.pi/4, np.pi/2, 3*np.pi/4]
 
-# this is defined here to be used in ML.py for feature_importance
-feature_headers = []
-for channel in channel_names:
-    for angle in angle_names:
-        for feature in feature_names:
-            feature_headers.append((feature + '-' + channel + '-' + angle))
+# global feature_headers
+feature_headers = [(feature + '-' + channel + '-' + angle) 
+                    for channel in channel_names 
+                    for angle in angle_names 
+                    for feature in feature_names]
 
+# value set to remove the background tiles that were incorrectly made by the grid_tiler()
+contrast_threshold = 30
 
+# CPU multiprocessing
 def feature_extraction(src, save_path):
     processes = []
     i = 1
@@ -47,6 +50,28 @@ def feature_extraction(src, save_path):
         process.join()
 
 
+# checks for background tiles that were incorretly made by the grid_tiler()
+def low_contrast_check(RGBimg):
+    Img_1 = RGBimg[:,:,0]
+    Img_2 = RGBimg[:,:,1]
+    Img_3 = RGBimg[:,:,2]
+
+    # creates the GLCM from each color channel
+    p_1 = skimage.feature.graycomatrix(np.array(Img_1), distances, angles, normed=True)
+    p_2 = skimage.feature.graycomatrix(np.array(Img_2), distances, angles, normed=True)
+    p_3 = skimage.feature.graycomatrix(np.array(Img_3), distances, angles, normed=True)
+
+    contrast_1 = glcm.graycoprops(p_1, prop='contrast')
+    contrast_2 = glcm.graycoprops(p_2, prop='contrast')
+    contrast_3 = glcm.graycoprops(p_3, prop='contrast')
+
+    if np.any(contrast_1 <= contrast_threshold) or np.any(contrast_2 <= contrast_threshold) or np.any(contrast_3 <= contrast_threshold):
+        return True
+    else:
+        return False
+
+
+# main()
 def texture_features(Folders_Path, save_path):
     if save_path.endswith('.csv') == True:
         pass
@@ -66,33 +91,14 @@ def texture_features(Folders_Path, save_path):
             if ultima_counter == 0:
                 Img = Image.open(os.path.join(Tiles_Path, file))
             ultima_counter+=1
-            
-    # finds the number of features through running feature extraction on the last tile in order to find other dimension for feature_matrix storage
-    RGBImg = np.asarray(Img)
-    feature_vector_RGB = calc_features(RGBImg, distances=distances, angles=angles)
-    feature_vector = feature_vector_RGB
-
-    # addition of HSV texture features
-    hsv_img = Img.convert('HSV')
-    hsv_img = np.array(hsv_img)
-    feature_vector_HSV = calc_features(hsv_img, distances=distances, angles=angles)
-    feature_vector = np.concatenate((feature_vector, feature_vector_HSV), axis=0)
-
-    # addition of LAB texture freatures 
-    lab_img = Img.convert('LAB')
-    lab_img = np.array(lab_img)
-    feature_vector_LAB = calc_features(lab_img, distances=distances, angles=angles)
-    feature_vector = np.concatenate((feature_vector, feature_vector_LAB), axis=0)
 
     # builds feature_matrix
-    feature_matrix = np.zeros([len(feature_vector),ultima_counter], dtype=object)
-
-    del feature_vector, feature_vector_RGB, feature_vector_HSV, feature_vector_LAB, Img, RGBImg, hsv_img
+    feature_matrix = np.zeros([len(feature_headers),ultima_counter], dtype=object)
     
     ultima_counter = 0
 
     for folder in os.listdir(Folders_Path):
-        tile_counter = 0
+        tile_counter = 0 # counts number of tiles per sample
         Tiles_Path = os.path.join(Folders_Path, folder)
 
         for file in os.listdir(Tiles_Path):
@@ -102,66 +108,83 @@ def texture_features(Folders_Path, save_path):
 
             image_path = os.path.join(Tiles_Path, file)
             Img = Image.open(image_path) 
-            RGBImg = np.asarray(Img)
+            Img_arr = np.asarray(Img)
             tile_name = 'Tile' + str(tile_counter)
-            RGBImg, BG_check = normalizeStaining(RGBImg[:,:,0:3], tile_name, saveFile=tile_name)
-            if BG_check == True:
+
+            # Normalizes tile to H&E standard and first place to catch background tiles that made it through
+            with warnings.catch_warnings():
+                warnings.filterwarnings('error')
+                try:
+                    RGBImg = normalizeStaining(Img_arr[:,:,0:3], tile_name, saveFile=tile_name)
+                except RuntimeWarning:
+                    tile_counter-=1
+                    ultima_counter-=1
+                    Img.close()
+                    # drop the last empty column of the feature matrix of zeros
+                    feature_matrix = feature_matrix[:,:-1]
+                    del Img, tile_name, Img_arr, image_path
+                    continue
+            
+            # second place to catch background tiles that made it through
+            low_contrast = low_contrast_check(Img_arr[:,:,0:3])
+            if low_contrast == True:
                 tile_counter-=1
                 ultima_counter-=1
+                Img.close()
+                # drop the last empty column of the feature matrix of zeros
+                feature_matrix = feature_matrix[:,:-1]
+                del Img, RGBImg, tile_name, Img_arr, image_path
                 continue
+            else:
+                RGBImg = Img_arr[:,:,0:3]
+            
+                # Texture features of RGB channels
+                feature_vector_RGB = calc_features(RGBImg, distances=distances, angles=angles)
+                feature_vector = feature_vector_RGB
 
-            feature_vector_RGB = calc_features(RGBImg, distances=distances, angles=angles)
-            # all color channels concatenated
-            feature_vector = feature_vector_RGB
+                # Texture features of HSV channels
+                hsv_img = Img.convert('HSV')
+                hsv_img = np.array(hsv_img)
+                feature_vector_HSV = calc_features(hsv_img, distances=distances, angles=angles)
+                feature_vector = np.concatenate((feature_vector, feature_vector_HSV), axis=0)
 
-            # Texture features of HSV channels
-            hsv_img = Img.convert('HSV')
-            hsv_img = np.array(hsv_img)
-            feature_vector_HSV = calc_features(hsv_img, distances=distances, angles=angles)
-            feature_vector = np.concatenate((feature_vector, feature_vector_HSV), axis=0)
+                # Texture features of LAB channels
+                lab_img = Img.convert('LAB')
+                lab_img = np.array(lab_img)
+                feature_vector_LAB = calc_features(lab_img, distances=distances, angles=angles)
+                feature_vector = np.concatenate((feature_vector, feature_vector_LAB), axis=0)
 
-            # Texture features of LAB channels
-            lab_img = Img.convert('LAB')
-            lab_img = np.array(lab_img)
-            feature_vector_LAB = calc_features(lab_img, distances=distances, angles=angles)
-            feature_vector = np.concatenate((feature_vector, feature_vector_LAB), axis=0)
+                # storing features within the matrix
+                feature_matrix[:, ultima_counter-1] = feature_vector
 
+                # frees up space by deleting variables
+                Img.close()
+                del feature_vector, Img, 
+                RGBImg, tile_name, hsv_img, 
+                feature_vector_RGB, feature_vector_HSV, feature_vector_LAB, lab_img, image_path
+                
+                # removes ext from file name before including it in dataframe
+                try:
+                    file_name = re.sub('.png$', '', file)
+                except:
+                    pass
+                try:
+                   file_name = re.sub('.tiff$', '', file)
+                except:
+                    pass
+                
+                try:
+                    index.append(file_name)
+                except:
+                    index.append(file)
 
-            # storing features within the matrix
-            feature_matrix[:, ultima_counter-1] = feature_vector
-
-            # frees up space by deleting variables
-            Img.close()
-            del feature_vector, Img, 
-            RGBImg, tile_name, BG_check, hsv_img, 
-            feature_vector_RGB, feature_vector_HSV, feature_vector_LAB, lab_img, image_path
-            try:
-                file_name = re.sub('.png$', '', file)
-            except:
-                pass
-            try:
-                file_name = file.re.sub('.tiff$', '', file)
-            except:
-                pass
-            try:
-                index.append(file_name)
-            except:
-                index.append(file)
-
-            elapsed_time = time.time() - time1
-            print('\nTile ' + str(ultima_counter) + '\n' + str(elapsed_time))
+                elapsed_time = time.time() - time1
+                print('\nTile ' + str(ultima_counter) + '\n' + str(elapsed_time))
         
     # convert to DataFrame with texture features
     feature_matrix = feature_matrix.T
-    
-    # create the columns for the data frame made from the feature_matrix array
-    columns = []
-    for channel in channel_names:
-        for angle in angle_names:
-            for feature in feature_names:
-                columns.append((feature + '-' + channel + '-' + angle))
 
-    feature_matrix = pd.DataFrame(feature_matrix, columns=columns)
+    feature_matrix = pd.DataFrame(feature_matrix, columns=feature_headers)
     
     feature_matrix['Tile'] = index
     feature_matrix.set_index('Tile', inplace=True)
